29947bde41656cbbffdfe2ef6e7c2d43b4006904
Alan Kan <alankan2004@gmail.com>
2020-07-05 18:19:25 -0400

    Modified class attribute names

--- supermemo2/sm_two.py
+++ supermemo2/sm_two.py
@@ -3,11 +3,11 @@ import datetime
 date_fmt = "%Y-%m-%d"
 
 class SMTwo:
-    def __init__(self, quality: int, prev_interval: int = 0, prev_repetitions: int = 0, prev_easiness: float = 2.5, first_visit: bool = False, last_review: datetime.date = datetime.date.today()):
+    def __init__(self, quality: int, interval: int = 0, repetitions: int = 0, easiness: float = 2.5, first_visit: bool = False, last_review: datetime.date = datetime.date.today()):
         self.quality = quality
-        self.prev_easiness = prev_easiness
-        self.prev_interval = prev_interval
-        self.prev_repetitions = prev_repetitions
+        self.easiness = easiness
+        self.interval = interval
+        self.repetitions = repetitions
         self.first_visit = first_visit
         self.last_review = last_review
 
@@ -31,18 +31,18 @@ class SMTwo:
         if self.quality < 3:
             self.new_repetitions = 0
             self.new_interval = 1
-            self.new_easiness = self.prev_easiness
+            self.new_easiness = self.easiness
         else:
-            if self.prev_repetitions == 0:
+            if self.repetitions == 0:
                 self.new_interval = 1
-            elif self.prev_repetitions == 1:
+            elif self.repetitions == 1:
                 self.new_interval = 6
             else:
-                self.new_interval = round(self.prev_interval * self.prev_easiness)
+                self.new_interval = round(self.interval * self.easiness)
 
-            self.new_repetitions = self.prev_repetitions + 1
+            self.new_repetitions = self.repetitions + 1
 
-            self.new_easiness = self.prev_easiness - 0.8 + 0.28 * self.quality - 0.02 * self.quality**2
+            self.new_easiness = self.easiness - 0.8 + 0.28 * self.quality - 0.02 * self.quality**2
 
         if self.new_easiness < 1.3:
             self.new_easiness = 1.3
@@ -59,25 +59,21 @@ class SMTwo:
 
     def invalid_input_handler(self):
         if self.first_visit:
-            if type(self.prev_interval) != int:
+            if type(self.interval) != int:
                 raise TypeError("Interval value should be an integer.")
-            elif self.prev_interval != 0:
+            elif self.interval != 0:
                 raise ValueError("Interval value should be 0 if this is the very first time.")
 
-            if type(self.prev_repetitions) != int:
+            if type(self.repetitions) != int:
                 raise TypeError("Repetitions value should be an integer.")
-            elif self.prev_repetitions != 0:
+            elif self.repetitions != 0:
                 raise ValueError("Repetitions value should be 0 if this is the very first time.")
 
-            if type(self.prev_easiness) != float:
+            if type(self.easiness) != float:
                 raise TypeError("Easiness value should be a float.")
-            elif self.prev_easiness != 2.5:
+            elif self.easiness != 2.5:
                 raise ValueError("Easiness value should start off with 2.5 if this is the very first time.")
         else:
-            if self.prev_easiness < 1.3:
+            if self.easiness < 1.3:
                 raise ValueError("Easiness value should not be smaller than 1.3!")
-
-if __name__ == "__main__":
-    sm_two = SMTwo(3)
-    sm_two.last_review = "2020-07-05"
-    sm_two.new_sm_two()
+            
