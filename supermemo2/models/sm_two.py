625c443da46dadc9c121ede920af9ed25c935d7d
alankan886 <alankan886@gmail.com>
2020-11-30 23:39:42 -0500

    :rotating_light: Add unit tests for sm_two model and implemented in TDD manner

--- supermemo2/models/sm_two.py
+++ supermemo2/models/sm_two.py
@@ -1,30 +1,47 @@
+import json
 from datetime import date, datetime, timedelta
 import warnings
 
 import attr
 
-from .prev import Prev
+from supermemo2.models.prev import Prev
 from supermemo2.exceptions import DateFormatError
 
 
 @attr.s()
 class SMTwo:
-    quality = attr.ib(converter=int)
+    # listener on quality and last_review is needed
+    quality = attr.ib()
     first_visit = attr.ib()
     easiness = attr.ib(default=2.5, converter=float)
-    interval = attr.ib(default=0, converter=int)
+    interval = attr.ib(default=1, converter=int)
     repetitions = attr.ib(default=1, converter=int)
     last_review = attr.ib(default=date.today())
     __next_review = attr.ib(init=False)
-    __old_values = attr.ib(factory=dict)
     __prev = attr.ib(init=False)
 
     __date_fmt = "%Y-%m-%d"
 
+    @quality.validator
+    def _check_quality(self, attribute, value):
+        # validator is after converter,
+        # using custom converter here to not convert bool to int to avoid confusing bugs,
+        # since int(True) = 1 and int(False) = 0.
+        if isinstance(value, bool):
+            message = f"{value} is not an integer"
+            raise TypeError(message)
+        else:
+            setattr(self, attribute.name, int(value))
+
+        if not 0 <= value <= 5:
+            message = f"{value} is not an integer between the range of 0 to 5"
+            raise ValueError(message)
+
     @first_visit.validator
     def _check_first_visit(self, attribute, value):
         if value is not True and value is not False:
-            raise ValueError(f"first_visit value, '{value}', is not a valid boolean value")
+            message = f"first_visit value, '{value}', is not a valid boolean value"
+            raise TypeError(message)
 
     @easiness.validator
     def _easiness_first_visit(self, attribute, value):
@@ -32,19 +49,49 @@ class SMTwo:
             setattr(self, attribute.name, 2.5)
             warnings.warn("easiness automatically set to 2.5, easiness should be 2.5 for the first visit")
 
+    @interval.validator
+    def _interval_first_visit(self, attribute, value):
+        if self.first_visit and value != 1:
+            setattr(self, attribute.name, 1)
+            warnings.warn("interval automatically set to 1, interval should be 1 for the first visit")
+
+    @repetitions.validator
+    def _repetitions_first_visit(self, attribute, value):
+        if self.first_visit and value != 1:
+            setattr(self, attribute.name, 1)
+            warnings.warn("repetitions automatically set to 1, repetitions should be 1 for the first visit")
+
     @last_review.validator
     def _convert_if_str(self, attribute, value):
-        if not isinstance(value, date) and isinstance(value, str):
+        if isinstance(value, str):
             try:
                 setattr(self, attribute.name, datetime.strptime(value, self.__date_fmt).date())
             except ValueError:
                 message = "please provide the last review date in the format of Year-Month-Day or an date object"
                 raise DateFormatError(message)
+        elif not isinstance(value, date):
+            message = "invalid type for last_review, which should be type Date or str"
+            raise TypeError(message)
 
     def __attrs_post_init__(self):
         self.__prev = Prev(self, self.easiness, self.interval, self.repetitions)
-        # take this out later
-        self.interval = 1
+        if self.quality < 3:
+            self.interval = 1
+            self.repetitions = 1
+        else:
+            if self.repetitions == 1:
+                pass
+            elif self.repetitions == 2:
+                self.interval = 6
+            else:
+                self.interval = round(self.interval * self.easiness)
+
+            self.repetitions += 1
+            self.easiness = self.easiness - 0.8 + 0.28 * self.quality - 0.02 * self.quality**2
+
+        if self.easiness < 1.3:
+            self.easiness = 1.3
+
         self.__next_review = self.last_review + timedelta(days=self.interval)
 
     @property
@@ -55,9 +102,15 @@ class SMTwo:
     def prev(self):
         return self.__prev
 
-
-if __name__ == "__main__":
-    sm = SMTwo(1, True)
-    print(sm.prev)
-    sm.prev.repetitions = 2
-    print(sm.interval)
+    def json(self):
+        return json.dumps(
+            {
+                "quality": self.quality,
+                "first_visit": self.first_visit,
+                "easiness": self.easiness,
+                "interval": self.interval,
+                "repetitions": self.repetitions,
+                "last_review": str(self.last_review),
+                "next_review": str(self.next_review),
+            }
+        )
--- tests/unit/test_sm_two.py
+++ tests/unit/test_sm_two.py
@@ -1,20 +1,122 @@
-from datetime import date, timedelta
+import json
+from datetime import date, datetime, timedelta
 
 import pytest
 
 from supermemo2 import SMTwo
+from supermemo2.exceptions import DateFormatError
 
 
-@pytest.mark.parametrize("quality_less_than_three", [0, 1, 2])
-def test_default_first_visit_q_less_than_three(quality_less_than_three):
-    sm_two = SMTwo(quality=quality_less_than_three, first_visit=True)
-    assert sm_two.quality == quality_less_than_three
+@pytest.mark.parametrize("bad_quality, error", [(False, TypeError), (6, ValueError), (-1, ValueError)])
+def test_quality_invalid_value(bad_quality, error):
+    with pytest.raises(error) as excinfo:
+        SMTwo(bad_quality, True)
+
+    if error == TypeError:
+        assert str(excinfo.value) == f"{bad_quality} is not an integer"
+    elif error == ValueError:
+        assert str(excinfo.value) == f"{bad_quality} is not an integer between the range of 0 to 5"
+
+
+@pytest.mark.parametrize("bad_first_visit", ["abc", 123, 0.01])
+def test_first_visit_invalid_value(bad_first_visit):
+    with pytest.raises(TypeError) as excinfo:
+        SMTwo(3, bad_first_visit)
+
+    assert str(excinfo.value) == f"first_visit value, '{bad_first_visit}', is not a valid boolean value"
+
+
+def test_last_review_convert_str_to_date():
+    sm_two = SMTwo(3, True, last_review="2020-01-01")
+    assert sm_two.last_review == datetime(2020, 1, 1).date()
+
+
+@pytest.mark.parametrize(
+    "bad_last_review, error",
+    [
+        ("123", DateFormatError),
+        ("01-01-2020", DateFormatError),
+        ("01/01/2020", DateFormatError),
+        (123, TypeError),
+        (123.0, TypeError)
+    ]
+)
+def test_last_review_invalid_value(bad_last_review, error):
+    with pytest.raises(error):
+        SMTwo(3, True, last_review=bad_last_review)
+
+
+# q < 3, no need to sepcify first visit
+@pytest.mark.parametrize(
+    "quality, first_visit",
+    [
+        (0, True),
+        (1, True),
+        (2, True),
+        (0, False),
+        (1, False),
+        (2, False)
+    ]
+)
+def test_q_less_than_three(quality, first_visit):
+    sm_two = SMTwo(quality, first_visit)
+    assert sm_two.quality == quality
+    assert sm_two.first_visit == first_visit
     assert sm_two.easiness == 2.5
     assert sm_two.interval == 1
     assert sm_two.repetitions == 1
     assert sm_two.last_review == date.today()
     assert sm_two.next_review == date.today() + timedelta(days=1)
 
+
+# q >= 3 first visit
+@pytest.mark.parametrize(
+    "quality, first_visit, easiness, interval, repetitions, expected_easiness, expected_interval, expected_repetitions",
+    [
+        (3, True, 2.5, 1, 1, 2.36, 1, 2),
+        (4, True, 2.5, 1, 1, 2.5000000000000004, 1, 2),
+        (5, True, 2.5, 1, 1, 2.6, 1, 2),
+        (3, False, 2.8, 1, 2, 2.6599999999999997, 6, 3),
+        (4, False, 2.8, 1, 2, 2.8000000000000003, 6, 3),
+        (5, False, 2.8, 1, 2, 2.9, 6, 3),
+        (3, False, 3.1, 6, 3, 2.9599999999999995, 19, 4),
+        (4, False, 3.1, 6, 3, 3.1, 19, 4),
+        (5, False, 3.1, 6, 3, 3.2, 19, 4),
+    ]
+)
+def test_q_larger_than_and_equal_to_three(quality, first_visit, easiness, interval, repetitions, expected_easiness, expected_interval, expected_repetitions):
+    sm_two = SMTwo(quality, first_visit, easiness, interval, repetitions)
+    assert sm_two.quality == quality
+    assert sm_two.first_visit == first_visit
+    assert sm_two.easiness == expected_easiness
+    assert sm_two.interval == expected_interval
+    assert sm_two.repetitions == expected_repetitions
+
+
+def test_easiness_lower_bound_reset():
+    """
+    Tests easiness resets to 1.3 when it goes below 1.3.
+    """
+    sm_two = SMTwo(3, False, 1.3)
+    assert sm_two.easiness == 1.3
+
+
+# test json method that returns all new information including quality, have the option of including first_visit or not
+def test_json():
+    sm_two = SMTwo(3, True)
+    today = date.today()
+    tomorrow = today + timedelta(days=1)
+    assert sm_two.json() == json.dumps(
+        {
+            "quality": 3,
+            "first_visit": True,
+            "easiness": 2.36,
+            "interval": 1,
+            "repetitions": 2,
+            "last_review": str(today),
+            "next_review": str(tomorrow)
+        }
+    )
 
 # @pytest.mark.parametrize("quality", [0, 1, 2, 3, 4, 5])
 # def test_prev(quality):
